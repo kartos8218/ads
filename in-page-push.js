6d2b78ac5556daeda119cee7971e41fcfcc967cc
Vipul Kumar <94109817+kartos8218@users.noreply.github.com>
2024-03-22 10:02:27 +0530

    Create in-page-push.js

---
+++ in-page-push.js
@@ -0,0 +1,1 @@
+    <script>(function(d,z,s){s.src='https://'+d+'/400/'+z;try{(document.body||document.documentElement).appendChild(s)}catch(e){}})('autchoog.net',6541000,document.createElement('script'))</script>
